0c510c7f1a1177826e1755cd2279759aac1bd969
flying_8lack <megacryleon@gmail.com>
2023-01-19 12:41:37 +0300

    added rocket model

--- src/main/java/com/flying_8lack/painmod/items/tools/DarkPickAxeItem.java
+++ src/main/java/com/flying_8lack/painmod/items/tools/DarkPickAxeItem.java
@@ -1,7 +1,12 @@
 package com.flying_8lack.painmod.items.tools;
 
 import com.flying_8lack.painmod.items.ModTier;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.PickaxeItem;
+import net.minecraft.world.level.Level;
 
 public class DarkPickAxeItem extends PickaxeItem {
     public DarkPickAxeItem(Properties pProperties) {
@@ -11,20 +16,8 @@ public class DarkPickAxeItem extends PickaxeItem {
     }
 
 
+    @Override
     public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pUsedHand) {
-
-
-        for(int x = 0; x < 8; x++){
-
-            BlockPos pos = new BlockPos(pPlayer.getLookAngle().scale((double)x));
-            BlockState state = pLevel.getBlockState(pos);
-
-
-            if(state.getBlock() == Blocks.STONE){
-                pLevel.destroyBlock(pos,true);
-                pLevel.destroyBlock(pos.below(),true);
-            }
-        }
         return super.use(pLevel, pPlayer, pUsedHand);
-    }*/
+    }
 }
