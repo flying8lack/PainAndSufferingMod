6edc8ca48a04c5f39654d8690f7a1240cd6e9059
flying_8lack <megacryleon@gmail.com>
2023-01-20 07:46:07 +0300

    Rocekt finished

--- src/main/java/com/flying_8lack/painmod/Entity/Rocket.java
+++ src/main/java/com/flying_8lack/painmod/Entity/Rocket.java
@@ -2,37 +2,32 @@ package com.flying_8lack.painmod.Entity;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.util.Mth;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.monster.Monster;
-import net.minecraft.world.entity.projectile.Arrow;
+import net.minecraft.world.entity.MoverType;
 import net.minecraft.world.entity.projectile.Projectile;
 import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.level.Explosion;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.BlockHitResult;
-import net.minecraft.world.phys.EntityHitResult;
 import net.minecraft.world.phys.HitResult;
 import net.minecraft.world.phys.Vec3;
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.core.PlayState;
-import software.bernie.geckolib3.core.builder.AnimationBuilder;
-import software.bernie.geckolib3.core.builder.ILoopType;
 import software.bernie.geckolib3.core.controller.AnimationController;
 import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
 import software.bernie.geckolib3.core.manager.AnimationData;
 import software.bernie.geckolib3.core.manager.AnimationFactory;
 import software.bernie.geckolib3.util.GeckoLibUtil;
 
-import javax.annotation.Nullable;
-
 public class Rocket extends Projectile implements IAnimatable {
 
 
     public AnimationFactory factory = GeckoLibUtil.createFactory(this);
-    private BlockPos pos;
+    private BlockPos pos = BlockPos.ZERO;
     private final int max_time = 20*8;
     private int life_time = 0;
-    private final float SPEED = 2.0f;
+    private final float SPEED = 5.0f;
     private float current_speed = SPEED;
 
     public Rocket(EntityType<? extends Projectile> pEntityType, Level pLevel) {
@@ -80,56 +75,88 @@ public class Rocket extends Projectile implements IAnimatable {
 
     }
 
+    public static void rotateTowardsMovement(Entity pProjectile, float pRotationSpeed) {
+        Vec3 vec3 = pProjectile.getDeltaMovement();
+        if (vec3.lengthSqr() != 0.0D) {
+            double d0 = vec3.horizontalDistance();
+            pProjectile.setYRot((float)(Mth.atan2(vec3.z, vec3.x) * (double)(180F / (float)Math.PI)) + 90.0F);
+            pProjectile.setXRot((float)(Mth.atan2(d0, vec3.y) * (double)(180F / (float)Math.PI)) - 90.0F);
+
+            while(pProjectile.getXRot() - pProjectile.xRotO < -180.0F) {
+                pProjectile.xRotO -= 360.0F;
+            }
+
+            while(pProjectile.getXRot() - pProjectile.xRotO >= 180.0F) {
+                pProjectile.xRotO += 360.0F;
+            }
+
+            while(pProjectile.getYRot() - pProjectile.yRotO < -180.0F) {
+                pProjectile.yRotO -= 360.0F;
+            }
+
+            while(pProjectile.getYRot() - pProjectile.yRotO >= 180.0F) {
+                pProjectile.yRotO += 360.0F;
+            }
+
+            pProjectile.setXRot(Mth.lerp(pRotationSpeed, pProjectile.xRotO, pProjectile.getXRot()));
+            pProjectile.setYRot(Mth.lerp(pRotationSpeed, pProjectile.yRotO, pProjectile.getYRot()));
+        }
+    }
+
     @Override
     public void tick() {
         //super.tick();
 
 
 
+
         Vec3 direction = new Vec3(this.getX() - this.pos.getX(),
                 this.getY() - this.pos.getY(),
                 this.getZ() - this.pos.getZ()).normalize().reverse();
-        double HorzDist = direction.horizontalDistance();
+        /*double HorzDist = direction.horizontalDistance();
         float roty = (float)(Mth.atan2(direction.y, HorzDist) * 180.0/Mth.PI);
         float rotx = (float)(Mth.atan2(direction.x,
-                direction.z) * 180.0/Mth.PI);
+                direction.z) * 180.0/Mth.PI);*/
 
 
-        //current_speed *= 0.995; //reduce the speed by 0.5% every tick
+        //current_speed *= 0.99; //reduce the speed by 1% every tick
 
 
-        this.setRot(roty, rotx);
-        this.setDeltaMovement(this.getDeltaMovement().lerp(
-                direction.scale(this.current_speed/20),0.95));
 
-        /*if(!this.isNoGravity()){
-            this.setDeltaMovement(this.getDeltaMovement().add(0, -0.2/20, 0));
-        }*/
+        this.setDeltaMovement(
+                direction.scale(this.current_speed/20));
+
+        if(!this.isNoGravity()){
+            this.setDeltaMovement(this.getDeltaMovement().add(0, -0.05, 0));
+        }
+        rotateTowardsMovement(this, 0.2f);
+        this.move(MoverType.SELF, this.getDeltaMovement());
+
+
+        //this.setRot(roty, rotx);
 
         this.tickDespawn();
-        if(current_speed <= 0.81f){
+        /*if(current_speed <= 0.81f){
             Explode();
-        }
+        }*/
 
 
 
         BlockHitResult pResult = this.getLevel().clip(new ClipContext(
                 this.position(), this.position().add(direction), ClipContext.Block.COLLIDER,
                 ClipContext.Fluid.NONE,null));
-        if(!this.level.isClientSide()){
-            if(pResult.getType() == HitResult.Type.ENTITY ||
-                    pResult.getType() == HitResult.Type.BLOCK) {
+        if (pResult.getType() == HitResult.Type.ENTITY ||
+                pResult.getType() == HitResult.Type.BLOCK) {
 
-                Explode();
-            }
-
+            Explode();
         }
 
     }
 
     public <E extends IAnimatable> PlayState predicate(AnimationEvent event){
-        event.getController().setAnimation(new AnimationBuilder().addAnimation("normal",
-                ILoopType.EDefaultLoopTypes.LOOP));
+        /*event.getController().setAnimation(new AnimationBuilder().addAnimation(
+                "animation.model.idle",
+                ILoopType.EDefaultLoopTypes.LOOP));*/
 
         return PlayState.CONTINUE;
     }
--- src/main/java/com/flying_8lack/painmod/items/tools/DarkPickAxeItem.java
+++ src/main/java/com/flying_8lack/painmod/items/tools/DarkPickAxeItem.java
@@ -3,6 +3,7 @@ package com.flying_8lack.painmod.items.tools;
 import com.flying_8lack.painmod.Entity.Rocket;
 import com.flying_8lack.painmod.ModEntity;
 import com.flying_8lack.painmod.items.ModTier;
+import net.minecraft.core.BlockPos;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
@@ -21,8 +22,8 @@ public class DarkPickAxeItem extends PickaxeItem {
     @Override
     public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pUsedHand) {
         Rocket r = new Rocket(ModEntity.ROCKET.get(), pLevel);
-        r.setTargetPos(pPlayer.getOnPos());
-        r.teleportTo(pPlayer.getX()+6, pPlayer.getY()+40, pPlayer.getZ()+6);
+        r.setTargetPos(new BlockPos(pPlayer.getEyePosition()));
+        r.teleportTo(pPlayer.getX()+5, pPlayer.getY()+30, pPlayer.getZ()+5);
         pLevel.addFreshEntity(r);
 
         return super.use(pLevel, pPlayer, pUsedHand);
